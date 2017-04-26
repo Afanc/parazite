8006e5d372d434e0afd4a53fee46731d27499ecc
Leo <leojequier@bluewin.ch>
2017-04-26 15:26:58 +0200

    modif de mardi dans main

--- src/main.py
+++ src/main.py
@@ -4,6 +4,26 @@
 from parazite1 import *
 from healthy import *
 
+nb_sains = 5
+nb_parasite = 3
+
+def start(nb_sains, nb_parasite):
+    indi = []
+    heal = []
+    para = []
+    for i in range(nb_sains):
+        heal.append(Healthy(200, 200)) # créer des individus
+    for a in heal:
+        indi.append(a)
+    for i in range(nb_parasite):
+        para.append(Parazite(5,6,1))
+    for i in range(1,10):
+        indi.append(i)
+    print len(heal)
+        
+
 def main():
     print 'le programme de sa mère\n'
-
+    start(nb_sains, nb_parasite)
+    
+main()
