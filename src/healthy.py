2d749a73e23de4ab624d94c59ad1d13c472c4328
Afanc <dariush.mollet@epfl.ch>
2017-04-26 13:17:27 +0200

    enlevé les tests

--- src/healthy.py
+++ src/healthy.py
@@ -11,17 +11,3 @@ class Healthy(Individual):  #est de type Individual
         Individual.__init__(self,pos,speed)     #et donc quand je crée un healthy, je crée un individu
         Resistances = []    #c'est un attribut que seuls les healthys ont uniquement
 
-
-
-
-
-print test.getSpeed()       #eh oui, ils existent et se partagent
-test2 = Healthy(2,4)           #créons un mec sain
-
-print isinstance(test2,Healthy)     #une fonction pratique, est-ce que a est un objet de type b ?
-print isinstance(test2,Individual)  #c'est un individu sain
-print isinstance(test, Healthy)     #c'est un individu non-sain
-print test2.position                #et donc j'ai une position, comme un individu
-
-
-from parazite1 import *     #voir commentaire dans parazite1.py
