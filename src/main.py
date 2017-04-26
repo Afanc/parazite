fda33ec244c2d5c50b1a6179f97b11e59943af04
Afanc <dariush.mollet@epfl.ch>
2017-04-26 13:19:09 +0200

    enlevé les tests

--- src/main.py
+++ src/main.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-                                                                                                
 #!/usr/bin/env python
 
-from individual import *
 from parazite1 import *
 from healthy import *
 
