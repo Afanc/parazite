f5cc7d98f01ed199aaa49cc9e2d2771c6d680979
Afanc <dariush.mollet@epfl.ch>
2017-04-28 16:01:20 +0200

    added const

---
+++ src/CONSTANTES.py
@@ -0,0 +1,11 @@
+#MAIN
+NB_SAINS = 5                                                                                                           
+NB_PARASITE = 3
+INFECT_CHANCE = 100
+CONTAINER_HEIGHT= 200
+CONTAINER_WIDTH= 300
+MAX_VELOCITY= 10
+MAX_VIRULANCE= 100
+
+#PARAZITE
+NUM_PAR = 100
