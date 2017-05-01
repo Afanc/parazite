873bb28fe400bbbae1281d8bbbc82848fe42d0c2
Afanc <dariush.mollet@epfl.ch>
2017-05-01 22:07:17 +0200

    tadaaaa trade off !

--- src/parazite1.py
+++ src/parazite1.py
@@ -39,20 +39,72 @@ class Parazite(Individual):
 
     #if vir goes up, transm rate goes up, recov goes down
     def set_New_Vir(self,r) : 
-        print 'todo'
+        new_fitness = self.getTransmRate() + self.getRecovProb() + r
+
+        diff = r - self.getVir()
+        s = sign(diff)
+
+        x = 0
+        y = 0
+
+        while(new_fitness > MAX_FITNESS) :
+	    
+	    max_x = (1 - s)/2 + s*self.getTransmRate()
+	    max_y = (1 - s)/2 + s*self.getRecovProb()
+
+            x = uniform(0, max_x) 	#on définit des pertes/gains aléatoires
+            y = uniform(0, max_y) 
+
+	    if r + self.getTransmRate()-s*x + self.getRecovProb()-s*y > MAX_FITNESS : 
+		norm = r/(x + y) 	#on essaie de normaliser
+		x = min(norm*x, max_x) 	#au cas où on dépasse la valeur max avec la normalisation
+		y = min(norm*y, max_y)
+
+	    new_fitness = r + self.getTransmRate()-x*s + self.getRecovProb()-y*s
+
+	self.setVir(r)
+        self.setTransmRate(self.getTransmRate() - s*x)
+        self.setRecovProb(self.getRecovProb() - s*y)
 
     #if transm rate goes up, vir goes up, recov goes down
     def set_New_TransmRate(self, r) :
-        print 'todo'
+        new_fitness = self.getVir() + self.getRecovProb() + r
+
+        diff = r - self.getTransmRate()
+        s = sign(diff)
+
+        x = 0
+        y = 0
+
+        while(new_fitness > MAX_FITNESS) :
+	    
+	    max_x = (1 - s)/2 + s*self.getVir()
+	    max_y = (1 - s)/2 + s*self.getRecovProb()
+
+            x = uniform(0, max_x) 	#on définit des pertes/gains aléatoires
+            y = uniform(0, max_y) 
+
+	    if r + self.getVir()-s*x + self.getRecovProb()-s*y > MAX_FITNESS : 
+		norm = r/(x + y) 	#on essaie de normaliser
+		x = min(norm*x, max_x) 	#au cas où on dépasse la valeur max avec la normalisation
+		y = min(norm*y, max_y)
+
+	    new_fitness = r + self.getVir()-x*s + self.getRecovProb()-y*s
+
+	self.setTransmRate(r)
+        self.setVir(self.getVir() - s*x)
+        self.setRecovProb(self.getRecovProb() - s*y)
+
 
     #if recov rate goes up, vir goes down, transm rate goes down
     def set_New_RecovProb(self, r) :
         new_fitness = self.getVir() + self.getTransmRate() + r
-        x = 0
-        y = 0
 
         diff = r - self.getRecovProb()
         s = sign(diff)
+        
+        x = 0
+        y = 0
 
         while(new_fitness > MAX_FITNESS) :
 	    
@@ -67,7 +119,6 @@ class Parazite(Individual):
 		x = min(norm*x, max_x) 	#au cas où on dépasse la valeur max avec la normalisation
 		y = min(norm*y, max_y)
 
-#different from parameter to parameter
 	    new_fitness = r + self.getVir()-x*s + self.getTransmRate()-y*s
 
 	self.setRecovProb(r)
@@ -85,21 +136,25 @@ print test.getTotalFitness()
 test.set_New_RecovProb(0.9)
 print test
 print test.getTotalFitness()
-#test.set_New_RecovProb(0.2)
-#test.set_New_RecovProb(0.3)
-#test.set_New_RecovProb(0.4)
-#test.set_New_RecovProb(0.5)
-#
-#test.set_New_TransmRate(0.2)
-#test.set_New_TransmRate(0.3)
-#test.set_New_TransmRate(0.4)
-#test.set_New_TransmRate(0.5)
-#test.set_New_TransmRate(0.6)
-#test.set_New_Vir(0.5)
-#test.set_New_Vir(0.4)
-#test.set_New_Vir(0.3)
-#test.set_New_Vir(0.2)
-#test.set_New_Vir(0.1)
+test.set_New_RecovProb(0.2)
+test.set_New_RecovProb(0.3)
+test.set_New_RecovProb(0.4)
+test.set_New_RecovProb(0.5)
+print test
+print test.getTotalFitness()
+
+test.set_New_TransmRate(0.2)
+test.set_New_TransmRate(0.3)
+test.set_New_TransmRate(0.4)
+test.set_New_TransmRate(0.5)
+test.set_New_TransmRate(0.6)
+test.set_New_Vir(0.5)
+test.set_New_Vir(0.4)
+test.set_New_Vir(0.3)
+test.set_New_Vir(0.2)
+test.set_New_Vir(0.1)
+print test
+print test.getTotalFitness()
 
 
     #idées
