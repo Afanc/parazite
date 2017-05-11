501f410893946c64ca43cba81d5010e36029d7d2
Afanc <dariush.mollet@gmail.com>
2017-05-11 15:31:33 +0200

    plot mieux

--- src/plot_that_shit.R
+++ src/plot_that_shit.R
@@ -16,8 +16,8 @@ if(length(args)!=1)
 }
 
 png(d)
-plot(data$virulence, type = "l",col='red')
+plot(data$virulence, type = "l",col='red', xlab = 'temps [sec]', ylab= 'mean value')
 lines(data$transmission, type = "l",col='blue')
 lines(data$guerison, type = "l",col='green')
-legend(0,0.1, legend = c("virulence", "transmission","guerison"), lty=c(1,1), col=c("red","blue", "green"))
+legend(0,0.2, legend = c("virulence", "transmission","guerison"), lty=c(1,1), col=c("red","blue", "green"))
 
