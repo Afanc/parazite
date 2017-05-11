85d5d9726f9ad9a0638509ead17076965ac27e01
Afanc <dariush.mollet@gmail.com>
2017-05-11 14:57:49 +0200

    plot mieux

--- src/plot_that_shit.R
+++ src/plot_that_shit.R
@@ -2,10 +2,22 @@
 
 data <- read.csv("data.csv", header=TRUE)
 head(data)
+args=(commandArgs(TRUE))
 
+if(length(args)!=1)
+{
+    print("Not enough/too many arguments supplied. Please supply d = filename")
+        q()
+}else
+{
+    for(i in 1:length(args)){
+        eval(parse(text=args[[i]]))
+    }
+}
+
+png(d)
 plot(data$virulence, type = "l",col='red')
 lines(data$transmission, type = "l",col='blue')
 lines(data$guerison, type = "l",col='green')
 legend(0,0.1, legend = c("virulence", "transmission","guerison"), lty=c(1,1), col=c("red","blue", "green"))
-
 
