ddca7df0cb5db2768366e15d31aec1a1ac2bed40
Leo <leojequier@bluewin.ch>
2017-05-12 14:50:58 +0200

    nouveau trade off avec un optimal et biologiquement correct

--- src/trade_off.py
+++ src/trade_off.py
@@ -1,16 +1,27 @@
 from parazite1 import *
 from random import uniform, sample
 import matplotlib.pyplot as plt
+from CONSTANTES import *
 
 def trade_off(para_i = None):
     if isinstance(para_i, Parazite):
         new_vir = 0
         new_recov = 0
         new_transmission = 0
-        effect = uniform(0,10)
-        new_vir = 1/(1+exp(-(effect/1.1-5)))
-        new_transmission = (effect**2)/100
-        new_recov = (effect**2) /100 
+        effect = (para_i.getVir()*100)**0.5
+        effect += uniform(-2,2)
+        compteur = 0
+        while effect > 10 or effect <0 and compteur < 3:
+            effect = (para_i.getVir()*100)**0.5
+            effect += uniform(-2,2)
+            compteur += 1
+        if effect > 10 or effect <0: 
+            effect = (para_i.getVir()*100)**0.5
+        new_vir = (effect**2)/100
+        new_transmission = 1/(1+exp(-(effect/1.1-5)))
+        new_recov =  0.1 + 1/2 * effect
+        if new_recov > 1:
+            new_recov = 1
         para_i.setVir(new_vir)
         para_i.setTransmRate(new_transmission)
         para_i.setRecovProb(new_recov)
@@ -19,18 +30,43 @@ def trade_off(para_i = None):
         new_recov = 0
         new_transmission = 0
         effect = uniform(0,10)
-        new_vir = 1/(1+exp(-(effect/1.1-5)))
-        new_transmission = (effect**2) /100 
-        new_recov =  (effect**2) /100 
+        new_vir = (effect**2)/100
+        new_transmission = 1/(1+exp(-(effect/1.1-5)))
+        new_recov =  0.1 + 1/2 * effect
+        if new_recov > 1:
+            new_recov = 1
+        
         return [new_vir,new_transmission,new_recov]
-    '''
+
+'''        
+y = []
+x = []
+R0 = 0
+for i in arange(0.1,10.0,0.1):
+    x.append(i)
+    effect = i
+    new_vir = (effect**2)/100
+    new_transmission = 1/(1+exp(-(effect/1.1-5)))
+    new_recov =  0.1 + 1/2 * effect
+    if new_recov > 1:
+        new_recov = 1
+    R0num = (100 * (1+new_transmission)*INFECTION_CHANCE)
+    R0den = (DYING_PROB + (1+new_vir)*DYING_PROB + (1+new_recov)*BASE_CHANCE_OF_HEALING)
+    R0 = R0num/R0den
+    y.append(R0)
+
+plt.scatter(x,y)
+
+plt.show()
+'''
+'''    
 new_vir = []    
 for effect in range(0,10,1):
     new_vir.append(1/(1+exp(-(effect/1.1-5))))
 plt.scatter(range(0,10,1 ),new_vir)
 plt.show()
-'''    
-
+  
+new_recov =  1- (effect**2)/150
     
 
 test = Parazite(0.7, 0.1, 0, 'ID23')    
@@ -38,3 +74,4 @@ trade_off(test)
 print test.getVir()
 print test.getTransmRate()
 print test.getRecovProb()
+'''
