18bbf403d5554a5b669ed0b1db053de544ec19bf
Afanc <dariush.mollet@gmail.com>
2017-05-15 04:01:37 +0200

    nice const

---
+++ src/nice_const_values/CONST_for_war.py
@@ -0,0 +1,52 @@
+# -*- coding: utf-8 -*-                                                                                                
+from random import *
+seed(42)
+#mode: 'theory_tester', 'war', 'all_night_long'
+MODE = 'war'
+
+#mode theory tester :
+SIMULATION_TIME = 120
+TEST_THEORY = 0
+
+#all_night_long
+#ALL_NIGHT_LONG = 0
+#HEALTHY_ROOF = 1
+STOCK_DYING_PROB = 0.13
+ROOF_DYING_PROB = 0.2
+STOCK_REPRODUCTION_PROB = 0.13
+BOTTOM_REPRODUCTION_PROB = 0.15
+
+#GUI
+DELTA_TIME = 1.0 / 60.0
+MAX_BALL_SPEED = 100
+BASE_COLOR = [0,0,1]
+
+#MAIN
+TRADE_OFF = 'dariush'
+NB_SAINS = 120
+NB_PARASITE = 10
+MAX_VELOCITY= 1
+MAX_VIRULANCE= 1
+MIN_SIZE_FOR_DATA = 40 * 1000  #pour avoir mesure en kb, à utiliser dans all_night_long et war
+
+#INDIVIDUAL
+DYING_PROB = 0.13    #max 0.5
+REPRODUCTION_PROB = 0.13
+
+#HEALTHY
+TRANSMISSION_RESISTANCE = 0
+
+#PARAZITE
+BASE_FITNESS = 0.8
+MAX_FITNESS = 1.8
+MAX_FITNESS_CHANGE_ON_INFECTION = 0.2   #max 0.5
+MAX_FITNESS_CHANGE_ON_NOTHING = 0.05   #max 0.5
+MAX_FITNESS_CHANGE_ON_REPRODUCTION = 0.2 #max 0.5
+
+#equations
+compteur_id = 1
+last_clock = 0
+nb_coll = 0
+mean_vir = 0
+mean_trans = 0
+mean_recov = 0
